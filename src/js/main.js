510cad1ab45f42c31b695f77b9f36dad78f96e72
viniseverodev <yvinisevero@gmail.com>
2025-04-02 21:50:40 -0300

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+import '../scss/style.scss';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+document.getElementById('newPostBtnInput').addEventListener('click', () => {
+  const modalElement = document.getElementById('postModal');
+  const postModal = new bootstrap.Modal(modalElement);
+  postModal.show();
+  setTimeout(() => {
+    document.getElementById('writePostInput').focus();
+  }, 500);
+});
+
+document.getElementById('themeSwitcher').addEventListener('click', () => {
+  const htmlElement = document.querySelector('html');
+  const iconElement = document.querySelector('#themeSwitcher>i');
+  if (htmlElement.dataset.bsTheme === 'dark') {
+    htmlElement.dataset.bsTheme = 'light';
+    iconElement.classList.remove('bi-moon-stars');
+    iconElement.classList.add('bi-brightness-high');
+  } else {
+    htmlElement.dataset.bsTheme = 'dark';
+    iconElement.classList.add('bi-moon-stars');
+    iconElement.classList.remove('bi-brightness-high');
+  }
+});
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.',  // A raiz do projeto é a pasta onde está o index.html
+  server: {
+    port: 8080
+  },
+  base: '/DevHub/',  // Certifica que o caminho base será a raiz do servidor
+});
